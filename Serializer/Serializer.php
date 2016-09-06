a50d8a4547df0d35305df927a1f66726b777f77c
qooplmao <fitz@michaelfitzpatrick.co.uk>
2016-09-06 10:03:07 +0100

    revert

--- Serializer/Serializer.php
+++ Serializer/Serializer.php
@@ -10,7 +10,6 @@
 
 namespace Mango\Bundle\JsonApiBundle\Serializer;
 
-use FOS\RestBundle\Serializer\Serializer as FOSSerialize;
 use JMS\Serializer\Exclusion\ExclusionStrategyInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\Serializer as BaseSerializer;
@@ -19,7 +18,7 @@ use Pagerfanta\Pagerfanta;
 /**
  * @author Steffen Brem <steffenbrem@gmail.com>
  */
-class Serializer extends BaseSerializer implements FOSSerialize
+class Serializer extends BaseSerializer
 {
     /**
      * @var ExclusionStrategyInterface[]
